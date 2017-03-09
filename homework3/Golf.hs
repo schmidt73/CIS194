f3c0ffb656083b2aa49ac3ef005b5dc884166f86
Henri Schmidt <packerfan73@gmail.com>
2017-03-09 13:26:48 -0500

    Finished hw 3.

--- homework3/Golf.hs
+++ homework3/Golf.hs
@@ -9,6 +9,22 @@ skip a n = case drop (n - 1) a of
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima (a:b@(c:d:_))
-    | (c > a) && (c > d) = c : (localMaxima b)
+    | c > a && c > d = c : localMaxima b
     | otherwise          = localMaxima b
 localMaxima _ = []
+
+histogram :: [Int] -> String
+histogram l = cHist (count l) "==========\n0123456789"
+
+cHist :: [Int] -> String -> String
+cHist (0:0:0:0:0:0:0:0:0:0:[]) s = s
+cHist l s = cHist (map (\x -> if x > 0 then x - 1 else 0) l) 
+                  ((map (\x -> if x > 0 then '*' else ' ') l) ++ "\n" ++ s)
+
+count :: [Int] -> [Int]
+count = hc (replicate 10 0)
+
+hc :: [Int] -> [Int] -> [Int]
+hc l (x:xs)  = let (a, b) = splitAt x l
+               in hc (a ++ [(head b) + 1] ++ tail b) xs
+hc l []      = l
