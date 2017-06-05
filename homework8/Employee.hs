949dbe20df8e1f67f7d3c38a7d55f94d77204a1a
Henri Schmidt <henri.schmidt@tufts.edu>
2017-06-05 16:50:51 +0000

    Finished hw8

--- homework8/Employee.hs
+++ homework8/Employee.hs
@@ -1,6 +1,9 @@
 module Employee where
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 import           Data.Tree
+import           Data.Monoid
 
 -- Employee names are represented by Strings.
 type Name = String
@@ -50,3 +53,36 @@ data GuestList = GL [Employee] Fun
 
 instance Ord GuestList where
   compare (GL _ f1) (GL _ f2) = compare f1 f2
+
+testGL1 = GL [(Emp "Stan" 9), (Emp "Bob" 3), (Emp "John" 1)] 13
+
+-- Exercise 1
+glCons :: Employee -> GuestList -> GuestList
+glCons e (GL es fs) = GL (e:es) (fs + empFun e)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL _ fs1) gl2@(GL _ fs2) 
+  | fs1 > fs2 = gl1
+  | otherwise = gl2
+
+instance Monoid (GuestList) where
+  mempty = GL [] 0
+  mappend (GL es1 fs1) (GL es2 fs2) = GL (es1 ++ es2) (fs1 + fs2)
+
+-- Exercise 2
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node a ts) = f a (map (treeFold f) ts)
+
+-- Exercise 3
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss bestLists = (maximumS withBossL, maximumS withoutBossL)
+  where withoutBossL   = map fst bestLists
+        withoutSubBoss = map snd bestLists
+        withBossL      = map (glCons boss) withoutSubBoss
+
+maximumS [] = mempty
+maximumS xs = maximum xs
+
+-- Exercise 4
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel
